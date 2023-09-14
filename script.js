46c3b69f951299c1966cd7f3d04600e8eea1b8c8
ZeroFPS-hk <ctckong@connect.ust.hk>
2023-09-14 22:41:30 +0100

    Add color change on hover canvas

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector(".canvas");
 
+window.onload = createCanvas(16);
+
 function createCanvas(size=16){
+    canvas.replaceChildren();
     let rowContainer, canvasSquare;
     for(let i=0; i<size; i++){
         rowContainer = document.createElement("div");
@@ -9,6 +12,7 @@ function createCanvas(size=16){
         for(let j=0; j<size; j++){
             canvasSquare = document.createElement("div");
             canvasSquare.classList.add("canvasSquare");
+            canvasSquare.addEventListener("mouseover", (e) => e.target.style.background="red");
             rowContainer.appendChild(canvasSquare);
         }
         canvas.appendChild(rowContainer);
