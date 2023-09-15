69202368d3c29f5d1c6924cc9d9c056adb8405df
ZeroFPS-hk <ctckong@connect.ust.hk>
2023-09-15 19:13:25 +0100

    Change canvas grid to fill different sizes (up to 100)

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ window.onmousedown = ()=> mouseDown = true;
 window.onmouseup = ()=> mouseDown = false;
 
 function createCanvas(size=16){
+    size = checkSize(size);
+    if(!size) return;
     canvas.replaceChildren();
     let rowContainer;
     for(let i=0; i<size; i++){
@@ -22,12 +24,28 @@ function createCanvas(size=16){
 
 function createCanvasSquare(rowContainer, size){
     const canvasSquare = document.createElement("div");
+    canvasSquare.style.width = `${canvas.clientWidth / size}px`;
+    canvasSquare.style.height = `${canvas.clientHeight / size}px`;
     canvasSquare.classList.add("canvasSquare");
     canvasSquare.addEventListener("mouseover", colorCanvasSquare);
     rowContainer.appendChild(canvasSquare);
 }
 
 function colorCanvasSquare(e){
-    if (mouseDown)
-        e.target.style.background = currentColor;
+    if(mouseDown) e.target.style.background = currentColor;
+}
+
+function checkSize(size){
+    if(isNaN(size)){
+        alert("Please enter a number.");
+        return false;
+    }
+
+    size = Math.floor(size);
+    if(size<=0){
+        alert("Please enter a positive interger.");
+        return false;
+    }
+
+    return size > 100? 100: size;
 }
