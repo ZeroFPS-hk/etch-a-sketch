5c4da5cdae1a62924dfc4a5220676afc2bc288bb
ZeroFPS-hk <ctckong@connect.ust.hk>
2023-09-15 18:32:28 +0100

    Change canvas to only fill when mouse is down and hovering

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const canvas = document.querySelector(".canvas");
+let currentColor = "red";
+let mouseDown = false;
 
 window.onload = createCanvas(16);
+window.onmousedown = ()=> mouseDown = true;
+window.onmouseup = ()=> mouseDown = false;
 
 function createCanvas(size=16){
     canvas.replaceChildren();
-    let rowContainer, canvasSquare;
+    let rowContainer;
     for(let i=0; i<size; i++){
         rowContainer = document.createElement("div");
         rowContainer.classList.add("rowContainer");
 
         for(let j=0; j<size; j++){
-            canvasSquare = document.createElement("div");
-            canvasSquare.classList.add("canvasSquare");
-            canvasSquare.addEventListener("mouseover", (e) => e.target.style.background="red");
-            rowContainer.appendChild(canvasSquare);
+            createCanvasSquare(rowContainer, size);
         }
         canvas.appendChild(rowContainer);
     }
+}
+
+function createCanvasSquare(rowContainer, size){
+    const canvasSquare = document.createElement("div");
+    canvasSquare.classList.add("canvasSquare");
+    canvasSquare.addEventListener("mouseover", colorCanvasSquare);
+    rowContainer.appendChild(canvasSquare);
+}
+
+function colorCanvasSquare(e){
+    if (mouseDown)
+        e.target.style.background = currentColor;
 }
